4796b3dabf107b4fdf71fadd6c8d972db126234c
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2025-09-09 11:45:34 +0200

    fastapi create and read

---
+++ code-alongs/08_fastapi_crud/constants.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import pandas as pd
+
+DATA_PATH = Path(__file__).parents[2] / "data"
+
+# print("-"*20)
+# print("\n"*5)
+
+# print(DATA_PATH)
+
+# print("\n"*5)
+# print("-"*20)
