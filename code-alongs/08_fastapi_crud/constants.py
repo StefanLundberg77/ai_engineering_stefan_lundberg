c8c17f91b8a817e7dc6202463935e929a9cf34b9
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2025-09-11 12:17:28 +0200

    glossary for ex.4

--- code-alongs/08_fastapi_crud/constants.py
+++ code-alongs/08_fastapi_crud/constants.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 DATA_PATH = Path(__file__).parents[2] / "data"
 
-# print("-"*20)
-# print("\n"*5)
+print("-"*20)
+print("\n"*5)
 
-# print(DATA_PATH)
+print(DATA_PATH)
 
-# print("\n"*5)
-# print("-"*20)
+print("\n"*5)
+print("-"*20)
+
