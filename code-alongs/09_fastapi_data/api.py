338b8c4767be8516a78323d635fbb5d495edb070
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2025-09-15 13:09:50 +0200

    fixed data_explorer bug mutation

--- code-alongs/09_fastapi_data/api.py
+++ code-alongs/09_fastapi_data/api.py
@@ -4,21 +4,24 @@ from data_processing import DataExplorer
 # from constants import DATA_PATH
 app = FastAPI()
 
-data_explorer = DataExplorer()
 
 @app.get("/api/sales")
 async def read_sales():
     # implement this code to return json data in this endpoint
+    data_explorer = DataExplorer()
+    
     return data_explorer.json_response()
 
 @app.get("/api/sales/summary")
 async def read_summary_data():
     """shows summary statistics"""
+    data_explorer = DataExplorer()
     return data_explorer.summary().json_response()
 
 @app.get("/api/sales/kpis")
 async def read_kpis_by_country(country: str):
     """kpis based on country"""
+    data_explorer = DataExplorer()
     return data_explorer.kpis(country=country) 
 
 # to run the API
