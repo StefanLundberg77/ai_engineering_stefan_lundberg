c49d437e00b545d81fa5c320fb64f3179841a621
stefan lundberg <bjorn.stefan.lundberg@gmail.com>
2025-09-15 13:12:25 +0200

    added router

--- code-alongs/09_fastapi_data/api.py
+++ code-alongs/09_fastapi_data/api.py
@@ -1,29 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from data_processing import DataExplorer
 
 # from constants import DATA_PATH
 app = FastAPI()
 
+router = APIRouter(prefix="/api/sales")
 
-@app.get("/api/sales")
+@app.router("")
 async def read_sales():
     # implement this code to return json data in this endpoint
     data_explorer = DataExplorer()
     
     return data_explorer.json_response()
 
-@app.get("/api/sales/summary")
+@app.router("/summary")
 async def read_summary_data():
     """shows summary statistics"""
     data_explorer = DataExplorer()
     return data_explorer.summary().json_response()
 
-@app.get("/api/sales/kpis")
+@app.router("/kpis")
 async def read_kpis_by_country(country: str):
     """kpis based on country"""
     data_explorer = DataExplorer()
     return data_explorer.kpis(country=country) 
 
+app.include_router(router)
 # to run the API
 # uvicorn api:app --reload
 
